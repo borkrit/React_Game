95419c6bba8e7883d23952476c572a3b1d57f4ae
vlad pohorilyi <vladyslav.pohorilyi@gmail.com>
2021-01-28 18:24:55 +0100

    create components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/Header'
+import Layout from './components/Layout';
+import Footer from './components/Footer';
+import bg1 from './assets/bg1.jpg'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const AppMain = ()=>{
+  let time = new Date().toLocaleTimeString();
+  return (
+    <React.Fragment>
+      <Header title='Hello' descr=' my name '/>
+      <Layout title='one' desc='wowowowoo' id= {time} urlBg={bg1}  />
+      <Layout title='two' desc='two' id= {time} colorBg='yellow' />
+      <Layout title='third' desc='third' id= {time} urlBg={bg1} />
+      <Footer />
+    </React.Fragment>
+  )
+}
+
+ReactDOM.render(<AppMain />, document.getElementById('root'));
+
