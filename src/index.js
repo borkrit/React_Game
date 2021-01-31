1d5b1365daf1cc7fa7eb90e20b359e5bc53c425d
vlad pohorilyi <vladyslav.pohorilyi@gmail.com>
2021-01-31 13:03:58 +0100

    create card pokemon, create function change side card

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './components/Header'
-import Layout from './components/Layout';
-import Footer from './components/Footer';
-import bg1 from './assets/bg1.jpg'
+import AppMain from './App';
 
 
-const AppMain = ()=>{
-  let time = new Date().toLocaleTimeString();
-  return (
-    <React.Fragment>
-      <Header title='Hello' descr=' my name '/>
-      <Layout title='one' desc='wowowowoo' id= {time} urlBg={bg1}  />
-      <Layout title='two' desc='two' id= {time} colorBg='yellow' />
-      <Layout title='third' desc='third' id= {time} urlBg={bg1} />
-      <Footer />
-    </React.Fragment>
-  )
-}
 
 ReactDOM.render(<AppMain />, document.getElementById('root'));
 
